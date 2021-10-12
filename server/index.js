bdc8d9f405d8bbaa3cc60ff5a94f76b816921ec1
Tony Kieling <tony.kieling@gmail.com>
2021-10-12 10:05:32 -0700

    handling dom elementns, trying cheerio

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-
 const fetch = require("node-fetch");
-const text = require("../output.js");
+const data = require("../output.js");
+const cheerio = require('cheerio');
 
 
 const jsdom = require("jsdom");
@@ -23,44 +23,86 @@ const stringToHTML = function (str) {
 };
 */
 
-// const getData = item => {
-//   return (
-//     {
-//       id: ,
-//       description
-//     }
-//   )
-// };
+const getData = item => {
+  // console.log("item", item);
+  // const $ = cheerio.load('<h2 class="title" href="asd">Hello world</h2>');
+  // console.log("item", $(".title").attr("href"));
 
-const f = async () => {
-  // const req = await fetch(
-  //   "https://vancouver.craigslist.org/search/apa?availabilityMode=0&lat=49.22828177157375&lon=-123.01000749085641&max_price=1850&min_price=1100&sale_date=all%20dates&search_distance=0.9",
+  const $ = cheerio.load(item);
+  console.log("item", $(".result-heading").attr("href"));
+  
+  // const tempItem = new JSDOM(item);
+  // console.log("tempItem", tempItem);
+  // console.log("item", tempItem.window.document.querySelector(".result-heading"));
+  return("a");
+  // return (
   //   {
-  //     method: "GET"
+  //     postId: ,
+  //     url: ,
+  //     description: ,
+  //     price
   //   }
   // );
-  // const data = await req.text();
+};
+
+const f = async () => {
+  // it gets data from craigslist and transform it into text
+//   const req = await fetch(
+//     "https://vancouver.craigslist.org/search/apa?availabilityMode=0&lat=49.22828177157375&lon=-123.01000749085641&max_price=1850&min_price=1100&sale_date=all%20dates&search_distance=0.9",
+//     {
+//       method: "GET"
+//     }
+//   );
+//   const data = await req.text();
+// console.log(data, data);
+
+  // // it converts the data into a tex format to a dom structure
+  // const dom = new JSDOM(data);
+
+  // // gets only the result to be handled
+  // const domElements = dom.window.document.querySelectorAll("li.result-row");
+
+  // console.log("domElements", domElements.length, domElements);
+  // //it gets 5 Nodelist items
+
+  const dom = cheerio.load(data);
+  const domElements = dom(".result-row");
+  console.log(domElements.length);
+  
+  const items=[];
+  for (let i = 0; i < domElements.length; i++) {
+    const tempData = getData(domElements[i]);
+    items.push(tempData);
+  }
+return;
 
 
-  const dom = new JSDOM(text.output());
-  // console.log("document:::", dom);
-  // const items = dom.window.document.getElementsByClassName("result-row");
-  const items = dom.window.document.querySelectorAll("li.result-row");
+  // const items = [];
+  domElements.forEach(e => {
+    const tempData = getData(e);
+    items.push(tempData);
+  });
+console.log("items==>", items);
 
-  console.log("items", items.length, items);
-  //it gets two Nodelist items
-  console.log(Array.from(items));
+  const id = dom.window.document.querySelector("#postid_7380919325").textContent;
+  console.log("id", id);
+  return;
+
 
   // from each item, get:
   // url, price and description
-  
   items.forEach(e => console.log("eee", e));
   for (let i = 0; i < items.length; i++){
-    // const item = new JSDOM(items[i]);
-
-    // console.log(items[i].window.document.querySelector("span.result-price").textContent);
+    // console.log(Array.from(items[i]));
+    // const temp = JSON.serialize(items[i]);
+    // console.log("temp", temp);
+    const item = new JSDOM(items[i]);
+    // return item;
+console.log("item", item)
+    // console.log(item.window.document.querySelector("span.result-price").innerHTML);
+    console.log(item.window.document.getElementById("#postid_7380919325").href);
   }
-  return;
+  // return;
   
   for (let i=0; i < items.length; i++) {
     const li = new JSDOM(items[i].innerHTML);
